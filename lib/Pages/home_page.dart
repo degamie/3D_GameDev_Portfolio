bc7fd3802c3e05d6830ce8d75a19750f14c6798f
Degamie_sign <106448560+degamie@users.noreply.github.com>
2024-11-01 16:26:39 +0530

    Add files via upload

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,66 +1,31 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends State<HomePage> {
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
+  State<HomePage> createState() => _HomePage();
+}
+
+class HomePage extends State<HomePage> {
+  const HomePage({super.key});
+  // const MyWidget({super.key});
   Widget build(BuildContext context) {
-    const _HomePage({super.key});
-    // const MyWidget({super.key});
-    Widget build(BuildContext context) {
-      return Scaffold(
-        body: ListView(         
-          scrollDirection: Axis.vertical,
-          children: [
+    return Scaffold(
+        body: ListView(
+      scrollDirection: Axis.vertical,
+      children: [
         //Main
         Container(
-          margin:const EdgeInsets.symmetric(
-            vertical:10.0,
-            horizontal: 20.0,
-          )
           height: 500,
           width: double.maxFinite,
           color: Colors.purple,
-          decoration: BoxDecoration(
-          gradient: LinearGradient(colors:[Colors.transparent,CustomColor.bgLight1,],
-          
-
-          )
-        ),
-           
-        child:Row(
-          children:[
-            const Text("Degamiesign Portfolio")
-            const Spacer(),
-            for(int i=0;i<5;i++)
-            TextButton(onPressed: (){}, child: const Text("Button"))
-            Text("DG",style: Text(
-              fontSize:23,
-              FontWeight:FontWeight.bold,
-              decoration:TextDecoration.underline,
-              color:customColor.purpleCombined
-
-            ))
-            Padding:const EdgeInsets.only(right:20),
-            decoration:KHederDecoration;
-            const SizedBox(width:15.10);
-          ],
-          )
         )
       ],
     ));
   }
 }
-}
-class _HomePage extends StatefulWidget {
-  const _HomePage();
 
-  @override
-  State<_HomePage> createState() => const _HomePage();
-}
-
-
-
-// class _HomePage extends State<_HomePage> {
-  
 
   // @override
   // Widget build(BuildContext context) {

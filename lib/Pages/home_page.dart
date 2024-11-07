79e5ccbb616504a9810f7b60eb6ad665440d802e
Degamie_sign <degamiesign@gmail.com>
2024-11-07 23:44:17 +0530

    Tenth Commit

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,40 +1,24 @@
 import 'package:flutter/material.dart';
 
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
-class HomePage extends State<HomePage> {
   @override
-  State<HomePage> createState() => _HomePage();
-
-}
-class HomePage extends StatefulWidget{
-  const HomePage({super.Key});
   Widget build(BuildContext context) {
     return Scaffold(
         body: ListView(
-      scrollDirection: Axis.vertical,
-      children: [
-        //Main
-        Container(
-          height: 500,
-          width: double.maxFinite,
-          color: Colors.purple,
-        )
-      ],
-    ));
-  }
+          scrollDirection: Axis.vertical,
+          children: [
+            //Main
+            Container(
+              height: 500,
+              width: double.maxFinite,
+              color: Colors.purple,
+            );
+          ]
+    }
 }
-// const MyWidget({super.key});
+class HomePage1 extends State<HomePage> {
+  HomePage createState() => HomePage();
+}
   // @override
-  // Widget build(BuildContext context) {
-  //   return const Placeholder();
-  // }
-
-// class HomePage extends StatefulWidget {
-
-
-//   @override
-//   State<StatefulWidget> createState() {
-//     // TODO: implement createState
-//     throw UnimplementedError();
-//   }
-// }

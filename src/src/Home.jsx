24003391b944497693e2a9a9cc486b885eaf73df
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-02-07 15:51:40 +0530

    Add files via upload

--- src/src/Home.jsx
+++ src/src/Home.jsx
@@ -2,8 +2,21 @@ import React, { Suspense } from 'react'
 
 function Home() {
   return (
+    const void AdjustPlaneForScreen(){
+      const screenScale=0;const screenPos=0;
+        if(window.ForScreenSize()){
+          screenScale=[1.5,1.5,1.5];
+          screenPos=[0,-1.5,0];
+          
+        }
+        else {
+          screenScale=[3,3,3];
+          screenPos=[0,-2.4,2.4];
+        }console.log(screenPos,rot);
+    }
     <div>Home</div>
     <section className="w-full h-screen relative">  </section>
+    <canvas className={'w-full h-screen bg-transparent}>camera={{near:.1,far:1000} ${isRotating ?cursor-grabbing,cursor,-grab'}></canvas>
         <canvas>camera={{near:.1,far:1000}}</canvas>
         <Suspense fallBack> {{Loader}}</Suspense>
         <hemisphereLight></hemisphereLight>

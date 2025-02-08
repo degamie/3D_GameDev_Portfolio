e9232d067099e3891de81def6e1f72a40fbfc24d
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-02-08 07:57:01 +0530

    Add files via upload

--- src/src/Home.jsx
+++ src/src/Home.jsx
@@ -1,9 +1,11 @@
 import React, { Suspense } from 'react'
 
 function Home() {
-  return (
-    const void AdjustPlaneForScreen(){
-      const screenScale=0;const screenPos=0;
+  return (    
+    <Plane planeScale={planeScale,planePosition}rotation={0,20,0}></Plane>
+ 
+    function AdjustPlaneForScreen(){
+      let screenScale=0;let screenPos=0;
         if(window.ForScreenSize()){
           screenScale=[1.5,1.5,1.5];
           screenPos=[0,-1.5,0];
@@ -14,13 +16,12 @@ function Home() {
           screenPos=[0,-2.4,2.4];
         }console.log(screenPos,rot);
     }
-    <div>Home</div>
-    <section className="w-full h-screen relative">  </section>
-    <canvas className={'w-full h-screen bg-transparent}>camera={{near:.1,far:1000} ${isRotating ?cursor-grabbing,cursor,-grab'}></canvas>
-        <canvas>camera={{near:.1,far:1000}}</canvas>
-        <Suspense fallBack> {{Loader}}</Suspense>
-        <hemisphereLight></hemisphereLight>
-    )
-}
+    <><Suspense fallBack> {{ Loader }}</Suspense><hemisphereLight></hemisphereLight></>
+  )
+    // <div>Home</div>
+    // <section className="w-full h-screen relative">  </section>
+    // <canvas className={'w-full h-screen bg-transparent}>camera={{near:.1,far:1000} ${isRotating ?cursor-grabbing,cursor,-grab'}></canvas>
+    // <canvas>camera={{near:.1,far:1000}}</canvas>
 
+}
 export default Home

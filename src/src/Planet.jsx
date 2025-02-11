cc2e2a485fbb9b5de42696557fba9b9c74bb02b5
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-02-11 20:31:44 +0530

    Add files via upload

---
+++ src/src/Planet.jsx
@@ -0,0 +1,27 @@
+import { useRef } from "react"
+
+const Planet({isRotating ,setIsRotating,...obj})=>{
+   const isPlanetRef=useRef();
+   const gl,viewPort=useThree();
+   const nodes,materials=useGLTF(isPlanetScene);
+   const lastX=useRef(0);
+   const dumpingSpeed=.95;
+
+   const handlingPtUp=(e)=>{
+    e.stopPropogation();
+    e.PreventDefault();
+    setIsRotating(false);
+
+   const handlingPt=(e)=>{
+    e.stopPropogation();
+    e.PreventDefault();
+    setIsRotating(true);
+    const clientX=e.touches?e.touches[0].clientX;
+    const lastX=e.touches?e.touches[0].clientX:e.clientX;
+    lastX.current=clientX;
+    const delta=clientX-lastCurrent/viewPort.width;
+    isPlanetRef.current.rotating+=delta*.95;
+    lastX.current*Math.PI.clientX;
+   }
+}
+}

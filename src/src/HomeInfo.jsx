1b3ada66ca5af17ced8664fb3324132910573b88
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-04 15:43:14 +0530

    Add files via upload

--- src/src/HomeInfo.jsx
+++ src/src/HomeInfo.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
 
 function HomeInfo() {
+  const renderCnt={
+    1:(
+      <h1>1</h1>
+    ),
+    2:(
+      <h1>2</h1>
+    ),
+    3:(
+      <h1>3</h1>
+    ),
+    4:(
+      <h1>4</h1>
+    )
+
+  }
   return (
     <div>GameDev HomeInfo</div>
   )

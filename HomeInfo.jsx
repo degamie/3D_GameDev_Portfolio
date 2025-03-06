bd3262ba8caf216d013258174784697a64830867
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-06 11:45:07 +0530

    Add files via upload

---
+++ HomeInfo.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function HomeInfo() {
+  const renderCnt={
+    1:(
+      <h1 className="sm:text-x1 sm:leading-snug text-centreneo -brutalline">1,This is DegamieSign WebPage
+      <span className="font-semibold">
+      <InfoBox=({texture,link,btnText})
+      <Linkto=({link})
+      </span></h1>
+    ),
+    2:(
+      <span>
+        <InfoBox/>Its a GameDev WebPage with Multi Sided Project currently working By (ME)=>Sarthak Mittal <YO!!!>
+          <Link ="/about">
+          <btnText>="Play Gamedev Mode"</btnText>
+          </span>
+          </YO!!!>
+      </span>
+      // <h1>2</h1>
+    ),
+    3:(
+      <h1>3</h1>
+    ),
+    4:(
+      <h1>4</h1>
+    )
+
+  }
+  return (
+    <div>GameDev HomeInfo</div>
+  )
+}
+
+export default HomeInfo

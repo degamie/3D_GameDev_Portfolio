60aed9b4a079f7364b2be63ed1b3c39519561f93
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-08 11:23:28 +0530

    Add files via upload

---
+++ src/src/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Contact() {
+  return (
+        <section className="relative flex-row flex-col max-container">
+            <div className="flext-1 min-w -[50%] flex-col"></div>
+            <h1 className="head-text">Keep In Touch</h1>
+            <form className="w-full flex flex-col gap-8 mt-15"></form>
+            <label className="text-black 500 font-semibold"></label>
+        </section>
+  )
+}
+
+export default Contact

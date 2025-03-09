aa82c9ea0851ab2f90ec9be2230ace2ebc2c6096
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-09 12:07:50 +0530

    Add files via upload

--- src/src/Contact.jsx
+++ src/src/Contact.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Contact() {
+  const [Form, setForm]=useState(name='',age='',gender='',email='',proffession='');
+  const handleChange()=>{};
+  const HandleFocus()=>{};
   return (
+    <input typeOnChange={handleChange}
+    OnFocus={HandleFocus}>
         <section className="relative flex-row flex-col max-container">
             <div className="flext-1 min-w -[50%] flex-col"></div>
             <h1 className="head-text">Keep In Touch</h1>
             <form className="w-full flex flex-col gap-8 mt-15"></form>
             <label className="text-black 500 font-semibold"></label>
         </section>
+        </input>
   )
 }
 

04972d576ced435ca6ebebcb09be0cae8aa56d66
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-20 22:08:29 +0530

    Add files via upload

--- src/src/HomeInfo.jsx
+++ src/src/HomeInfo.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
-
+import React, { useLayoutEffect } from 'react'
+//Home Info Funct 
 function HomeInfo() {
+  const val=0;
+  useLayoutEffect(()={
+   console.Log("Home Info Val",val); 
+    ([val]);
+  });
+  setTimeout(()=>{
+    setVal("GameDev Portfolio");
+  }3000);
   const renderCnt={
     1:(
       <h1>1</h1>

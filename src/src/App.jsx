1e79cfad285f45e4df47af86a4e6dc95cfdeeebc
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-04-08 09:36:27 +0530

    Add files via upload

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
-function App() {
+class App extends Home,HomeInfo,Contact{
+  function App() {
   return (
     <h1 className="text-3X1'font-bold underlineText-red-400"></h1>
     <div>App</div>
   )
+}
 }
 
 export default App

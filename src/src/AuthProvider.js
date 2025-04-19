9aaea30fd4ffa2000c117e0e62eabf7a1a6ca7a8
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-04-19 10:05:11 +0530

    Add files via upload

--- src/src/AuthProvider.js
+++ src/src/AuthProvider.js
@@ -1,5 +1,23 @@
+import { useEffect } from "react";
+
 class AuthProvider extends Home{
     const AuthProvider=({children})=>{
         return <AuthContext.Provider>{children}</AuthContext.Provider>;
     }
+        function AppNavigation(){
+            const {isAuthenticated,login,logout}=useAuth();
+            useEffect(()=>{
+                bootstrapAsync=async()=>{
+                    setLoading(true);
+                    const userAttributes=null;
+                    try{
+                        user=await checkAuth();
+                        const {attributes:userAttr}=user;
+                        userAttributes=userAttr;
+                    }
+                }
+            })
+
+        }
+
 }

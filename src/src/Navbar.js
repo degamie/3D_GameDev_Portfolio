882832b2b4b18d0f908b66af7a3067ad73fc021a
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-04-22 09:38:07 +0530

    Add files via upload

---
+++ src/src/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+class Navbar extends AuthProvider{
+export const Navbar=()=>{
+    const {user}=useAuth();
+}
+}
+export default Navbar;
+// function Navbar() {
+//   return (
+//     <div>Navbar</div>
+//   )
+// }

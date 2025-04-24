d5660f7d7da090b4e6e9555109aadd435e5a4d1d
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-04-24 09:14:13 +0530

    Add files via upload

---
+++ src/src/RoutesPath.jsx
@@ -0,0 +1,7 @@
+const UserProfile(var usernName){//Fetching UsrProfile
+    const {userName}=useParams();
+    return  <div>UserProfile:<userName></userName></div> 
+}
+return <Route>
+    <Route path="/" element={<Home/>}/>
+</Route>

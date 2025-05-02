a6b53e734c7b9b451fa3d0699f1f1960594a524f
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-05-02 09:36:36 +0530

    Add files via upload

--- src/src/Authorization.jsx
+++ src/src/Authorization.jsx
@@ -1,6 +1,15 @@
-const {user}=useAuth();
+function Authorization{
+    const {user}=useAuth();
 const {location}=uselocation();
 if(user.usernane){
     userpermission=user.userpermission;
     const isAllowed=Permissions.some(allowed)=>userpermission.includes((allowed));
+}
+var Password=null;
+if(Password.length===null)return null;
+while(Password.length!==null){
+    if(Password.length>0)Password.length+=1;
+    else Password.length=0;
+}return Password;
+
 }

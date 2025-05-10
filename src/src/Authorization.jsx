29adad38928ce78257464fad6a1367767ba10ab1
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-05-10 11:40:15 +0530

    Add files via upload

--- src/src/Authorization.jsx
+++ src/src/Authorization.jsx
@@ -3,7 +3,7 @@ function Authorization{
 const {location}=uselocation();
 if(user.usernane){
     userpermission=user.userpermission;
-    const isAllowed=Permissions.some(allowed)=>userpermission.includes((allowed));
+    Boolean isAllowed=Permissions.some(allowed)=>userpermission.includes((allowed));
 }
 var Password=null;
 if(Password.length===null)return null;
@@ -11,7 +11,17 @@ while(Password.length!==null){
     if(Password.length>0)Password.length+=1;
     else Password.length=0;
 }return Password;
+function Logout(const isAllowed,var userPermission,var Password,var username){//Logout funct declare
+    var len=username.length;//Username's Length Declare
+    var len1=Password.length;//Password's Length Declare
+    if(userPermission===null)return 0;//Base cond
+    while(isAllowed!==false){//iterating through usr's Allowable permission
+        else if(len>0){//Checking len Greater Val
+            len-=len1;//username and password's Dec Length
+        }
+    }console.log("User has Been logged Out");//printing User's Logged Out
 }
+
 const Loginbtn=()=>{
     const {LoginbtnwithRedirect}=useAuth0();
     return (
@@ -21,4 +31,5 @@ const Loginbtn=()=>{
             }
         </button>
     )
+}
 }

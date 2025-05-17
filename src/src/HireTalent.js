355cb9a0388f7a84757f9d590b8aa9ae1714185c
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-05-17 20:08:12 +0530

    Add files via upload

---
+++ src/src/HireTalent.js
@@ -0,0 +1,11 @@
+class HireTalent{
+        public let name=null;
+       public let age=null;
+        public let gender=null;
+        public let work_exp=null;
+        public let college=null;
+        public let course=null;
+        public let Projects=null;HireTalent(){}
+Resume_Upload(){}//To Be Implemented 
+
+}
